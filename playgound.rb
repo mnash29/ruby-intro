99c7ee6c22dff15c384469cc671a63e6f08bbfd5
mnash29 <mtntrades10@gmail.com>
2019-05-28 22:16:46 +0000

    Intro to ruby started

---
+++ playgound.rb
@@ -0,0 +1,13 @@
+# Say hello-world in different ways
+# Simple put string
+# Using a variable
+# Using a method
+
+puts "Working with Ruby"
+# Calls 'puts' method which prints the string and returns 'nil'
+
+p "Working with Ruby"
+# Sends string to 'p' method and returns the same argument string instead 
+# of 'nil'
+
+# print "Working with Ruby" works the same as 'p' but without newline
